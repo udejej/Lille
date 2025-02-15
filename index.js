b639b0d2ae8a8a7067885bff7d597a17c256aa92
udejej <gabriel4545@outlook.fr>
2025-02-15 14:30:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Joue à Lille Roleplay", "Belignouf le triso"];
+const statusMessages = ["Lille Interactive", "Créer un ticket si problème avec WL & Verif"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
